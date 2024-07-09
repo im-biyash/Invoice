fbe738bdba4edf7e206c4d3a201d93521eaca59a
biyash <bsstha21@gmail.com>
2024-07-09 17:52:36 +0545

    cahnges

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // Scan all HTML files in the root directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
